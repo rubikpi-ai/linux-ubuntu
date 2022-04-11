320bbe9bc7f99fbe5a434b4d98befa2cdc8d73ca
Kai-Heng Feng <kai.heng.feng@canonical.com>
2022-04-11 17:24:32 +0800

    UBUNTU: SAUCE: PCI/ASPM: Enable ASPM for links under VMD domain
    BugLink: https://bugs.launchpad.net/bugs/1942160
    
    New Intel laptops with VMD cannot reach deeper power saving state,
    renders very short battery time.
    
    As BIOS may not be able to program the config space for devices under
    VMD domain, ASPM needs to be programmed manually by software. This is
    also the case under Windows.
    
    The VMD controller itself is a root complex integrated endpoint that
    doesn't have ASPM capability, so we can't propagate the ASPM settings to
    devices under it. Hence, simply apply ASPM_STATE_ALL to the links under
    VMD domain, unsupported states will be cleared out anyway.
    
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -6289,3 +6289,15 @@ static void pci_fixup_d3cold_delay_1sec(struct pci_dev *pdev)
 	pdev->d3cold_delay = 1000;
 }
 DECLARE_PCI_FIXUP_FINAL(0x5555, 0x0004, pci_fixup_d3cold_delay_1sec);
+
+/*
+ * Device [8086:9a09]
+ * BIOS may not be able to access config space of devices under VMD domain, so
+ * it relies on software to enable ASPM for links under VMD.
+ */
+static void pci_fixup_enable_aspm(struct pci_dev *pdev)
+{
+	pdev->dev_flags |= PCI_DEV_FLAGS_ENABLE_ASPM;
+}
+DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_INTEL, 0x9a09, pci_fixup_enable_aspm);
+DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_INTEL, 0xa0b0, pci_fixup_enable_aspm);
