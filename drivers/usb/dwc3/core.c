9e97786e68537cc8e36bca27ec65e9502cb7c46b
Bjorn Andersson <quic_bjorande@quicinc.com>
2023-10-16 20:11:13 -0700

    FROMLIST: usb: dwc3: Override end of dwc3 memory resource
    In the case that the dwc3 core driver is instantiated from the same
    memory resource information as the glue driver, the dwc_res memory
    region will overlap with the memory region already mapped by the glue.
    
    As the DWC3 core driver already does math on the passed memory region to
    exclude the XHCI region, also adjust the end address, to avoid having to
    pass an adjusted region from the glue explicitly.
    
    Link: https://lore.kernel.org/all/20231016-dwc3-refactor-v1-5-ab4a84165470@quicinc.com/ .
    
    Change-Id: Id303516131943e74da9f3e1d54ad50eb441b131d
    Signed-off-by: Bjorn Andersson <quic_bjorande@quicinc.com>
    Acked-by: Konrad Dybcio <konrad.dybcio@linaro.org>
    Signed-off-by: Krishna Kurapati <quic_kriskura@quicinc.com>
    (cherry picked from commit e3e6a469ebaf8e6c8a02012089daa00feb5b3fb2)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1979,6 +1979,7 @@ struct dwc3 *dwc3_probe(struct platform_device *pdev)
 	 */
 	dwc_res = *res;
 	dwc_res.start += DWC3_GLOBALS_REGS_START;
+	dwc_res.end = res->start + DWC3_OTG_REGS_END;
 
 	if (dev->of_node) {
 		struct device_node *parent = of_get_parent(dev->of_node);
@@ -1986,6 +1987,7 @@ struct dwc3 *dwc3_probe(struct platform_device *pdev)
 		if (of_device_is_compatible(parent, "realtek,rtd-dwc3")) {
 			dwc_res.start -= DWC3_GLOBALS_REGS_START;
 			dwc_res.start += DWC3_RTK_RTD_GLOBALS_REGS_START;
+			dwc_res.end = dwc_res.start + DWC3_OTG_REGS_END;
 		}
 
 		of_node_put(parent);
