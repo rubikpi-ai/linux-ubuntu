99874e6788c50b5f6e3a46858da7bac639455405
John Ogness <john.ogness@linutronix.de>
2023-10-20 10:03:42 +0000

    printk: nbcon: Add unsafe flushing on panic
    BugLink: https://bugs.launchpad.net/bugs/2060704
    
    Add nbcon_atomic_flush_unsafe() to flush all nbcon consoles
    using the write_atomic() callback and allowing unsafe hostile
    takeovers. Call this at the end of panic() as a final attempt
    to flush any pending messages.
    
    Note that legacy consoles use unsafe methods for flushing
    from the beginning of panic (see bust_spinlocks()). Therefore,
    systems using both legacy and nbcon consoles may still fail to
    see panic messages due to unsafe legacy console usage.
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Kevin Becker <kevin.becker@canonical.com>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -198,6 +198,7 @@ void printk_trigger_flush(void);
 bool pr_flush(int timeout_ms, bool reset_on_progress);
 extern void nbcon_driver_acquire(struct console *con);
 extern void nbcon_driver_release(struct console *con);
+void nbcon_atomic_flush_unsafe(void);
 #else
 static inline __printf(1, 0)
 int vprintk(const char *s, va_list args)
@@ -288,6 +289,10 @@ static inline void nbcon_driver_acquire(struct console *con)
 }
 
 static inline void nbcon_driver_release(struct console *con)
+{
+}
+
+static inline void nbcon_atomic_flush_unsafe(void)
 {
 }
 
