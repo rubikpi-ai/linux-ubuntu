e28ed934deda7295e9bfeb0783f9c1d8abc6a84e
Taniya Das <quic_tdas@quicinc.com>
2023-10-31 23:56:38 +0530

    PENDING: clk: qcom: lpassaudiocc: Add support to skip PLL configuration
    On certain targets the PLL configuration should be skipped, thus add a
    device property to support the same.
    
    Change-Id: I0fa8ca17cb488513dc1663aef64bb4f15f1db1f5
    Signed-off-by: Taniya Das <quic_tdas@quicinc.com>
    (cherry picked from commit a2f16f07238c9ac2d638c58594229a45223cb56f
    https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/clk/qcom/lpassaudiocc-sc7280.c
+++ drivers/clk/qcom/lpassaudiocc-sc7280.c
@@ -781,17 +781,22 @@ static int lpass_audio_cc_sc7280_probe(struct platform_device *pdev)
 		goto exit;
 	}
 
-	clk_zonda_pll_configure(&lpass_audio_cc_pll, regmap, &lpass_audio_cc_pll_config);
+	if (!of_property_read_bool(pdev->dev.of_node, "qcom,adsp-skip-pll")) {
+		clk_zonda_pll_configure(&lpass_audio_cc_pll, regmap, &lpass_audio_cc_pll_config);
 
-	/* PLL settings */
-	regmap_write(regmap, 0x4, 0x3b);
-	regmap_write(regmap, 0x8, 0xff05);
+		/* PLL settings */
+		regmap_write(regmap, 0x4, 0x3b);
+		regmap_write(regmap, 0x8, 0xff05);
+	}
 
 	ret = qcom_cc_really_probe(&pdev->dev, desc, regmap);
 	if (ret) {
 		dev_err(&pdev->dev, "Failed to register LPASS AUDIO CC clocks\n");
 		goto exit;
 	}
+
+	lpass_audio_cc_sc7280_regmap_config.name = "lpassaudio_cc_reset";
+	lpass_audio_cc_sc7280_regmap_config.max_register = 0xc8;
 
 	ret = qcom_cc_probe_by_index(pdev, 1, &lpass_audio_cc_reset_sc7280_desc);
 	if (ret) {
