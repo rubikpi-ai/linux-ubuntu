483c1ca83736aff59141bccc7f772e921541cdee
Seshu Madhavi Puppala <quic_spuppala@quicinc.com>
2023-12-19 08:02:31 +0530

    FROMLIST: firmware: qcom_scm: scm call for deriving a software secret
    Fix the type issue for software secret call.Fix parameter
    names to improve readability.
    
    This patch aligns with the updated v3 version of the upstream
    patch mentioned in the below link.
    
    Signed-off-by: Gaurav Kashyap <quic_gaurkash@quicinc.com>
    Link: https://lore.kernel.org/linux-arm-msm/20231122053817.3401748-1-quic_gaurkash@quicinc.com/.
    
    Change-Id: Icae8cf0b120db2e0f77cd53c5ec1b3254024deb0
    Signed-off-by: Seshu Madhavi Puppala <quic_spuppala@quicinc.com>

--- include/linux/firmware/qcom/qcom_scm.h
+++ include/linux/firmware/qcom/qcom_scm.h
@@ -105,9 +105,8 @@ bool qcom_scm_ice_available(void);
 int qcom_scm_ice_invalidate_key(u32 index);
 int qcom_scm_ice_set_key(u32 index, const u8 *key, u32 key_size,
 			 enum qcom_scm_ice_cipher cipher, u32 data_unit_size);
-int qcom_scm_derive_sw_secret(const u8 *wrapped_key,
-			      u32 wrapped_key_size, u8 *sw_secret,
-			      u32 secret_size);
+int qcom_scm_derive_sw_secret(const u8 *wkey, size_t wkey_size,
+			      u8 *sw_secret, size_t sw_secret_size);
 
 int qcom_scm_generate_ice_key(u8 *lt_key, size_t lt_key_size);
 int qcom_scm_prepare_ice_key(const u8 *lt_key, size_t lt_key_size,
