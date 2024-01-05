5d0fbc244bff66e190734644fb0923483a63acfa
Swee Leong Ching <leong.ching.swee@intel.com>
2024-01-05 15:09:23 +0800

    net: stmmac: Make MSI interrupt routine generic
    There is no support for per DMA channel interrupt for non-MSI platform,
    where the MAC's per channel interrupt hooks up to interrupt controller(GIC)
    through shared peripheral interrupt(SPI) to handle interrupt from TX/RX
    transmit channel.
    
    This patch generalize the existing MSI ISR to also support non-MSI
    platform.
    
    Signed-off-by: Teoh Ji Sheng <ji.sheng.teoh@intel.com>
    Signed-off-by: Swee Leong Ching <leong.ching.swee@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 477bd4beb93bf9ace9bda71f1437b191befa9cf4)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-socfpga.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-socfpga.c
@@ -429,6 +429,9 @@ static int socfpga_dwmac_probe(struct platform_device *pdev)
 
 	plat_dat->riwt_off = 1;
 
+	if (stmmac_res.rx_irq[0] > 0 && stmmac_res.tx_irq[0] > 0)
+		plat_dat->flags |= STMMAC_FLAG_MULTI_IRQ_EN;
+
 	ret = stmmac_dvr_probe(&pdev->dev, plat_dat, &stmmac_res);
 	if (ret)
 		return ret;
