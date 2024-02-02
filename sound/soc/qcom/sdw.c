54967764d4f034834b5e363d399d6335b50ccd8c
Manikantan R <quic_manrav@quicinc.com>
2024-02-02 17:45:42 +0530

    PENDING: ASoC: qcom: qcm6490: Add handling for WSA Codec DMA TX
    Handle WSA Codec DMA TX case during init & prepare calls.
    
    Change-Id: I55eb2400207cd350c00ac583ee1c9563a22428c4
    Signed-off-by: Manikantan R <quic_manrav@quicinc.com>
    (cherry picked from commit ca34120deef8fb55a0a8469e58f5f00743c34be7
     https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- sound/soc/qcom/sdw.c
+++ sound/soc/qcom/sdw.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (c) 2018-2023, Linaro Limited.
 // Copyright (c) 2018, The Linux Foundation. All rights reserved.
+// Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
 
 #include <dt-bindings/sound/qcom,q6afe.h>
 #include <linux/module.h>
@@ -64,6 +65,7 @@ int qcom_snd_sdw_prepare(struct snd_pcm_substream *substream,
 	switch (cpu_dai->id) {
 	case WSA_CODEC_DMA_RX_0:
 	case WSA_CODEC_DMA_RX_1:
+	case WSA_CODEC_DMA_TX_0:
 	case RX_CODEC_DMA_RX_0:
 	case RX_CODEC_DMA_RX_1:
 	case TX_CODEC_DMA_TX_0:
@@ -113,6 +115,7 @@ int qcom_snd_sdw_hw_params(struct snd_pcm_substream *substream,
 
 	switch (cpu_dai->id) {
 	case WSA_CODEC_DMA_RX_0:
+	case WSA_CODEC_DMA_TX_0:
 	case RX_CODEC_DMA_RX_0:
 	case RX_CODEC_DMA_RX_1:
 	case TX_CODEC_DMA_TX_0:
@@ -141,6 +144,7 @@ int qcom_snd_sdw_hw_free(struct snd_pcm_substream *substream,
 	switch (cpu_dai->id) {
 	case WSA_CODEC_DMA_RX_0:
 	case WSA_CODEC_DMA_RX_1:
+	case WSA_CODEC_DMA_TX_0:
 	case RX_CODEC_DMA_RX_0:
 	case RX_CODEC_DMA_RX_1:
 	case TX_CODEC_DMA_TX_0:
