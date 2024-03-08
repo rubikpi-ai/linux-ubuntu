26f64161400e942ff65b9359a2be9b3fbdbff49b
Rohit kumar <quic_rohkumar@quicinc.com>
2024-03-08 15:02:54 +0530

    QCLINUX: ASoC: qcom: qdsp6: Fix compilation warnings
    Include local header files with "" to avoid compilation
    warnings.
    
    Change-Id: I597f94bfc26a5a4d623201089d12520750cb0d45
    Signed-off-by: Rohit kumar <quic_rohkumar@quicinc.com>
    (cherry picked from commit c31a00d2c1abc04723f313b84eb1380f90dab8f4)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- sound/soc/qcom/qdsp6/msm_audio_mem.c
+++ sound/soc/qcom/qdsp6/msm_audio_mem.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/init.h>
@@ -29,6 +29,7 @@
 #include <linux/platform_device.h>
 #include <linux/firmware/qcom/qcom_scm.h>
 #include <soc/qcom/secure_buffer.h>
+#include "msm_audio_mem.h"
 
 MODULE_IMPORT_NS(DMA_BUF);
 
