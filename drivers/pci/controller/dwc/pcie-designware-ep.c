01c4ce816e3d00a1a13fb0b1bd8d17c905c059a2
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2024-03-27 14:43:32 +0530

    PCI: dwc: ep: Remove deinit() callback from struct dw_pcie_ep_ops
    deinit() callback was solely introduced for the pcie-rcar-gen4 driver where
    it is used to do platform specific resource deallocation. And this callback
    is called right at the end of the dw_pcie_ep_exit() API. So it doesn't
    matter whether it is called within or outside of dw_pcie_ep_exit() API.
    
    So let's remove this callback and directly call rcar_gen4_pcie_ep_deinit()
    in pcie-rcar-gen4 driver to do resource deallocation after the completion
    of dw_pcie_ep_exit() API in rcar_gen4_remove_dw_pcie_ep().
    
    This simplifies the DWC layer.
    
    Link: https://lore.kernel.org/linux-pci/20240327-pci-dbi-rework-v12-3-082625472414@linaro.org
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Reviewed-by: Niklas Cassel <cassel@kernel.org>
    Reviewed-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    (cherry picked from commit b7dec6b85089fcadf2478e22429d4e1863f95294)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/pci/controller/dwc/pcie-designware-ep.c
+++ drivers/pci/controller/dwc/pcie-designware-ep.c
@@ -666,9 +666,6 @@ void dw_pcie_ep_deinit(struct dw_pcie_ep *ep)
 			      epc->mem->window.page_size);
 
 	pci_epc_mem_exit(epc);
-
-	if (ep->ops->deinit)
-		ep->ops->deinit(ep);
 }
 EXPORT_SYMBOL_GPL(dw_pcie_ep_deinit);
 
@@ -874,7 +871,7 @@ int dw_pcie_ep_init(struct dw_pcie_ep *ep)
 			       ep->page_size);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize address space\n");
-		goto err_ep_deinit;
+		return ret;
 	}
 
 	ep->msi_mem = pci_epc_mem_alloc_addr(epc, &ep->msi_mem_phys,
@@ -910,10 +907,6 @@ err_free_epc_mem:
 
 err_exit_epc_mem:
 	pci_epc_mem_exit(epc);
-
-err_ep_deinit:
-	if (ep->ops->deinit)
-		ep->ops->deinit(ep);
 
 	return ret;
 }
