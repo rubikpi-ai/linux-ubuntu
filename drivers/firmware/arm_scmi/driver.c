49b90f5eda1aaee6eab9921d90997760dbe79d3f
Peng Fan <peng.fan@nxp.com>
2024-04-18 09:32:28 +0800

    firmware: arm_scmi: Introduce get_max_msg_size() helper/accessor
    When the agent is sending data to the SCMI platform, the drivers in the
    agent could check the maximum message size supported to avoid potential
    protocol buffer overflow.
    
    Introduce the helper/accessor function get_max_msg_size() for the same.
    
    Reviewed-by: Cristian Marussi <cristian.marussi@arm.com>
    Reviewed-by: Dhruva Gole <d-gole@ti.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Link: https://lore.kernel.org/r/20240418-pinctrl-scmi-v11-1-499dca9864a7@nxp.com
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>
    (cherry picked from commit 4869b5cc9641c0cdc100315f324a73f64576ee66)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/firmware/arm_scmi/driver.c
+++ drivers/firmware/arm_scmi/driver.c
@@ -1539,6 +1539,20 @@ out:
 }
 
 /**
+ * scmi_common_get_max_msg_size  - Get maximum message size
+ * @ph: A protocol handle reference.
+ *
+ * Return: Maximum message size for the current protocol.
+ */
+static int scmi_common_get_max_msg_size(const struct scmi_protocol_handle *ph)
+{
+	const struct scmi_protocol_instance *pi = ph_to_pi(ph);
+	struct scmi_info *info = handle_to_scmi_info(pi->handle);
+
+	return info->desc->max_msg_size;
+}
+
+/**
  * scmi_protocol_msg_check  - Check protocol message attributes
  *
  * @ph: A reference to the protocol handle.
@@ -1856,6 +1870,7 @@ static void scmi_common_fastchannel_db_ring(struct scmi_fc_db_info *db)
 
 static const struct scmi_proto_helpers_ops helpers_ops = {
 	.extended_name_get = scmi_common_extended_name_get,
+	.get_max_msg_size = scmi_common_get_max_msg_size,
 	.iter_response_init = scmi_iterator_init,
 	.iter_response_run = scmi_iterator_run,
 	.protocol_msg_check = scmi_protocol_msg_check,
