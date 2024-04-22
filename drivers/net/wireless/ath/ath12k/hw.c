1481e652a069f70120309b1916f054a66d90366d
Lingbo Kong <quic_lingbok@quicinc.com>
2024-04-22 11:30:50 +0800

    wifi: ath12k: ACPI TAS support
    Currently, ath12k does not support Time-Average-SAR (TAS). In order to enable
    TAS read the tables from ACPI and send them to the firmware using
    WMI_PDEV_SET_BIOS_INTERFACE_CMDID command. Besides, ath12k registers an ACPI
    event callback so that ACPI can notify ath12k to get the updated SAR power
    table and sends it to the firmware when the device state is changed.
    
    ACPI is only enabled for WCN7850 using struct ath12k_hw_params::acpi_guid
    field. Most likely QCN9274 will never support ACPI as the chip is not used in
    laptops.
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0-03427-QCAHMTSWPL_V1.0_V2.0_SILICONZ-1.15378.4
    
    Signed-off-by: Lingbo Kong <quic_lingbok@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://msgid.link/20240422033054.979-2-quic_lingbok@quicinc.com
    (cherry picked from commit 576771c9fa21a38bcf390b5ecc610441574e5014)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/net/wireless/ath/ath12k/hw.c
+++ drivers/net/wireless/ath/ath12k/hw.c
@@ -15,6 +15,10 @@
 #include "mhi.h"
 #include "dp_rx.h"
 
+static const guid_t wcn7850_uuid = GUID_INIT(0xf634f534, 0x6147, 0x11ec,
+					     0x90, 0xd6, 0x02, 0x42,
+					     0xac, 0x12, 0x00, 0x03);
+
 static u8 ath12k_hw_qcn9274_mac_from_pdev_id(int pdev_idx)
 {
 	return pdev_idx;
@@ -950,6 +954,8 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 
 		.supports_sta_ps = false,
 
+		.acpi_guid = NULL,
+
 		.iova_mask = 0,
 	},
 	{
@@ -1025,6 +1031,8 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 
 		.supports_sta_ps = true,
 
+		.acpi_guid = &wcn7850_uuid,
+
 		.iova_mask = ATH12K_PCIE_MAX_PAYLOAD_SIZE - 1,
 	},
 	{
@@ -1094,6 +1102,8 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.otp_board_id_register = QCN9274_QFPROM_RAW_RFA_PDET_ROW13_LSB,
 
 		.supports_sta_ps = false,
+
+		.acpi_guid = NULL,
 
 		.iova_mask = 0,
 	},
