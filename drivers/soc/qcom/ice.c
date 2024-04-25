2b7ee1bf62b7164d9e89117e0878082e728c2880
Seshu Madhavi Puppala <quic_spuppala@quicinc.com>
2024-04-25 16:42:23 +0530

    PENDING: soc: qcom: ice: Handle ice probe return
    Clients like UFS/eMMC call of_qcom_ice_get() to get a ICE handle.
    Currently the return value of ICE probe is causing issue of client probe.
    Return -EOPNOTSUPP in certain specific cases so that clients may optionally
    disable ICE and proceed further.
    
    Change-Id: Ia31e2660a5f11937542b8d4b8f91c69bfd4192c8
    Signed-off-by: Seshu Madhavi Puppala <quic_spuppala@quicinc.com>
    (cherry picked from commit 78746ba239bd132a5af8926b2bd5544ba32564d8)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/soc/qcom/ice.c
+++ drivers/soc/qcom/ice.c
@@ -69,6 +69,8 @@
 #define qcom_ice_readl(engine, reg)	\
 	readl((engine)->base + (reg))
 
+static bool qcom_ice_create_error;
+
 struct qcom_ice {
 	struct device *dev;
 	void __iomem *base;
@@ -522,7 +524,7 @@ static struct qcom_ice *qcom_ice_create(struct device *dev,
 
 	if (!qcom_scm_ice_available()) {
 		dev_warn(dev, "ICE SCM interface not found\n");
-		return NULL;
+		return ERR_PTR(-EOPNOTSUPP);
 	}
 
 	engine = devm_kzalloc(dev, sizeof(*engine), GFP_KERNEL);
@@ -603,7 +605,7 @@ struct qcom_ice *of_qcom_ice_get(struct device *dev)
 	 */
 	node = of_parse_phandle(dev->of_node, "qcom,ice", 0);
 	if (!node)
-		return NULL;
+		return ERR_PTR(-EOPNOTSUPP);
 
 	pdev = of_find_device_by_node(node);
 	if (!pdev) {
@@ -617,7 +619,10 @@ struct qcom_ice *of_qcom_ice_get(struct device *dev)
 		dev_err(dev, "Cannot get ice instance from %s\n",
 			dev_name(&pdev->dev));
 		platform_device_put(pdev);
-		ice = ERR_PTR(-EPROBE_DEFER);
+		if (qcom_ice_create_error)
+			ice = ERR_PTR(-EOPNOTSUPP);
+		else
+			ice = ERR_PTR(-EPROBE_DEFER);
 		goto out;
 	}
 
@@ -632,7 +637,6 @@ struct qcom_ice *of_qcom_ice_get(struct device *dev)
 
 out:
 	of_node_put(node);
-
 	return ice;
 }
 EXPORT_SYMBOL_GPL(of_qcom_ice_get);
@@ -696,8 +700,11 @@ static int qcom_ice_probe(struct platform_device *pdev)
 	}
 
 	engine = qcom_ice_create(&pdev->dev, base);
-	if (IS_ERR(engine))
+
+	if (IS_ERR(engine)) {
+		qcom_ice_create_error = true;
 		return PTR_ERR(engine);
+	}
 
 	platform_set_drvdata(pdev, engine);
 
