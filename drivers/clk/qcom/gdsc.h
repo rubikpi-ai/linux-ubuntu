68e4fc0e6af6bddb0702e1d66a3e9dcae5b0a588
Jagadeesh Kona <quic_jkona@quicinc.com>
2024-05-24 12:08:13 +0530

    PENDING: clk: qcom: gdsc: Add support to link multiple PDs as GDSC parents
    A GDSC within a clock controller can have multiple power domains
    associated with it for it's proper functionality. Add support to link
    multiple power domains as GDSC parents so all the required power
    domains gets enabled before the gdsc_enable() and disabled after the
    gdsc_disable().
    
    Change-Id: If5a40f68f6843ebfab40b789127cf28cadc3c697
    Signed-off-by: Jagadeesh Kona <quic_jkona@quicinc.com>
    Co-developed-by: Taniya Das <quic_tdas@quicinc.com>
    Signed-off-by: Taniya Das <quic_tdas@quicinc.com>
    (cherry picked from commit 2d64626e295759b2fc5adf706b261d68ccae3975)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/clk/qcom/gdsc.h
+++ drivers/clk/qcom/gdsc.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2015, 2017-2018, 2022, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __QCOM_GDSC_H__
@@ -78,6 +79,7 @@ struct gdsc {
 struct gdsc_desc {
 	struct device *dev;
 	struct gdsc **scs;
+	struct dev_pm_domain_list *pmdomains;
 	size_t num;
 };
 
