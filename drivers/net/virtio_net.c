c030602b10e52dcd65b295b90f2f3b3d7c550111
Jason Wang <jasowang@redhat.com>
2024-08-14 13:22:28 +0800

    virtio-net: synchronize probe with ndo_set_features
    BugLink: https://bugs.launchpad.net/bugs/2097301
    
    [ Upstream commit c392d6019398315526b0b508282f87c7b2318c72 ]
    
    We calculate guest offloads during probe without the protection of
    rtnl_lock. This lead to race between probe and ndo_set_features. Fix
    this by moving the calculation under the rtnl_lock.
    
    Fixes: 3f93522ffab2 ("virtio-net: switch off offloads on demand if possible on XDP set")
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Link: https://patch.msgid.link/20240814052228.4654-5-jasowang@redhat.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -4923,6 +4923,11 @@ static int virtnet_probe(struct virtio_device *vdev)
 		netif_carrier_on(dev);
 	}
 
+	for (i = 0; i < ARRAY_SIZE(guest_offloads); i++)
+		if (virtio_has_feature(vi->vdev, guest_offloads[i]))
+			set_bit(guest_offloads[i], &vi->guest_offloads);
+	vi->guest_offloads_capable = vi->guest_offloads;
+
 	rtnl_unlock();
 
 	err = virtnet_cpu_notif_add(vi);
@@ -4930,11 +4935,6 @@ static int virtnet_probe(struct virtio_device *vdev)
 		pr_debug("virtio_net: registering cpu notifier failed\n");
 		goto free_unregister_netdev;
 	}
-
-	for (i = 0; i < ARRAY_SIZE(guest_offloads); i++)
-		if (virtio_has_feature(vi->vdev, guest_offloads[i]))
-			set_bit(guest_offloads[i], &vi->guest_offloads);
-	vi->guest_offloads_capable = vi->guest_offloads;
 
 	pr_debug("virtnet: registered device %s with %d RX and TX vq's\n",
 		 dev->name, max_queue_pairs);
