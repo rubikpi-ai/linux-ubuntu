0ec7a2bf7a08b44c2fd0a71d2652132bff05f445
Florian Fainelli <florian.fainelli@broadcom.com>
2024-08-27 11:24:50 -0700

    BACKPORT: UPSTREAM: firmware: arm_scmi: Support 'reg-io-width' property for shared memory
    Some shared memory areas might only support a certain access width,
    such as 32-bit, which memcpy_{from,to}_io() does not adhere to at least
    on ARM64 by making both 8-bit and 64-bit accesses to such memory.
    
    Update the shmem layer to support reading from and writing to such
    shared memory area using the specified I/O width in the Device Tree. The
    various transport layers making use of the shmem.c code are updated
    accordingly to pass the I/O accessors that they store.
    
    Git-commit: 2cd7f3db25feeb7c204e36df9f1bb13bea3a3a20
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Change-Id: I90b2c9f06b8b3d1ec5da47990f417416826dbb77
    Signed-off-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Message-Id: <20240827182450.3608307-3-florian.fainelli@broadcom.com>
    Signed-off-by: Deepti Jaggi <quic_djaggi@quicinc.com>
    (cherry picked from commit 8eeca884dc4619e0cf3b12b610c00d86126362c6
    https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/firmware/arm_scmi/transports/mailbox.c
+++ drivers/firmware/arm_scmi/transports/mailbox.c
@@ -26,6 +26,7 @@
  * @cinfo: SCMI channel info
  * @shmem: Transmit/Receive shared memory area
  * @chan_lock: Lock that prevents multiple xfers from being queued
+ * @io_ops: Transport specific I/O operations
  */
 struct scmi_mailbox {
 	struct mbox_client cl;
@@ -35,6 +36,7 @@ struct scmi_mailbox {
 	struct scmi_chan_info *cinfo;
 	struct scmi_shared_mem __iomem *shmem;
 	struct mutex chan_lock;
+	struct scmi_shmem_io_ops *io_ops;
 };
 
 #define client_to_scmi_mailbox(c) container_of(c, struct scmi_mailbox, cl)
@@ -45,7 +47,8 @@ static void tx_prepare(struct mbox_client *cl, void *m)
 {
 	struct scmi_mailbox *smbox = client_to_scmi_mailbox(cl);
 
-	core->shmem->tx_prepare(smbox->shmem, m, smbox->cinfo);
+	core->shmem->tx_prepare(smbox->shmem, m, smbox->cinfo,
+				smbox->io_ops->toio);
 }
 
 static void rx_callback(struct mbox_client *cl, void *m)
@@ -71,7 +74,7 @@ static void rx_callback(struct mbox_client *cl, void *m)
 	}
 
 	core->rx_callback(smbox->cinfo,
-			core->shmem->read_header(smbox->shmem), NULL);
+		      core->shmem->read_header(smbox->shmem), NULL);
 }
 
 static bool mailbox_chan_available(struct device_node *of_node, int idx)
@@ -199,7 +202,8 @@ static int mailbox_chan_setup(struct scmi_chan_info *cinfo, struct device *dev,
 	if (!smbox)
 		return -ENOMEM;
 
-	smbox->shmem = core->shmem->setup_iomap(cinfo, dev, tx, NULL);
+	smbox->shmem = core->shmem->setup_iomap(cinfo, dev, tx, NULL,
+						&smbox->io_ops);
 	if (IS_ERR(smbox->shmem))
 		return PTR_ERR(smbox->shmem);
 
@@ -307,7 +311,7 @@ static void mailbox_fetch_response(struct scmi_chan_info *cinfo,
 {
 	struct scmi_mailbox *smbox = cinfo->transport_info;
 
-	core->shmem->fetch_response(smbox->shmem, xfer);
+	core->shmem->fetch_response(smbox->shmem, xfer, smbox->io_ops->fromio);
 }
 
 static void mailbox_fetch_notification(struct scmi_chan_info *cinfo,
@@ -315,7 +319,8 @@ static void mailbox_fetch_notification(struct scmi_chan_info *cinfo,
 {
 	struct scmi_mailbox *smbox = cinfo->transport_info;
 
-	core->shmem->fetch_notification(smbox->shmem, max_len, xfer);
+	core->shmem->fetch_notification(smbox->shmem, max_len, xfer,
+					smbox->io_ops->fromio);
 }
 
 static void mailbox_clear_channel(struct scmi_chan_info *cinfo)
