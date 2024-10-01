73485197eb1b992012fbae221e3e868c1a1a33c3
Abhinav Kumar <quic_abhinavk@quicinc.com>
2024-10-01 15:25:18 -0700

    FROMLIST: drm/msm/dp: make bridge helpers use dp_display to allow re-use
    dp_bridge helpers take drm_bridge as an input and extract the
    dp_display object to be used in the dp_display module. Rather than
    doing it in a roundabout way, directly pass the dp_display object
    to these helpers so that the MST bridge can also re-use the same
    helpers.
    
    Change-Id: Id2862dcd05dad8b0b9d00d5e955a6c4b37dd8736
    Link: https://lore.kernel.org/all/20241205-dp_mst-v1-12-f8618d42a99a@quicinc.com/
    Signed-off-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Signed-off-by: Yongxing Mou <quic_yongmou@quicinc.com>
    (cherry picked from commit e2fd439c76a99ef38103a377796a68329a886f3d)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/gpu/drm/msm/dp/dp_drm.h
+++ drivers/gpu/drm/msm/dp/dp_drm.h
@@ -25,18 +25,6 @@ int msm_dp_bridge_init(struct msm_dp *msm_dp_display, struct drm_device *dev,
 		   struct drm_encoder *encoder,
 		   bool yuv_supported);
 
-void msm_dp_bridge_atomic_enable(struct drm_bridge *drm_bridge,
-			     struct drm_bridge_state *old_bridge_state);
-void msm_dp_bridge_atomic_disable(struct drm_bridge *drm_bridge,
-			      struct drm_bridge_state *old_bridge_state);
-void msm_dp_bridge_atomic_post_disable(struct drm_bridge *drm_bridge,
-				   struct drm_bridge_state *old_bridge_state);
-enum drm_mode_status msm_dp_bridge_mode_valid(struct drm_bridge *bridge,
-					  const struct drm_display_info *info,
-					  const struct drm_display_mode *mode);
-void msm_dp_bridge_mode_set(struct drm_bridge *drm_bridge,
-			const struct drm_display_mode *mode,
-			const struct drm_display_mode *adjusted_mode);
 void msm_dp_bridge_hpd_enable(struct drm_bridge *bridge);
 void msm_dp_bridge_hpd_disable(struct drm_bridge *bridge);
 void msm_dp_bridge_hpd_notify(struct drm_bridge *bridge,
