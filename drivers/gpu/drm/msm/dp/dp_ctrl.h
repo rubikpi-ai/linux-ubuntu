43c80f6bfa351dc37925cd14d36222177aeaa83c
Abhinav Kumar <quic_abhinavk@quicinc.com>
2024-10-02 11:19:49 -0700

    FROMLIST: drm/msm/dp: allow dp_ctrl stream APIs to use any panel passed to it
    Currently, the dp_ctrl stream APIs operate on their own dp_panel
    which is cached inside the dp_ctrl's private struct. However with MST,
    the cached panel represents the fixed link and not the sinks which
    are hotplugged. Allow the stream related APIs to work on the panel
    which is passed to them rather than the cached one. For SST cases,
    this shall continue to use the cached dp_panel.
    
    Change-Id: I27f8f5cb73d55f01ab23d41da5152b5108475fa3
    Link: https://lore.kernel.org/all/20241205-dp_mst-v1-9-f8618d42a99a@quicinc.com/
    Signed-off-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Signed-off-by: Yongxing Mou <quic_yongmou@quicinc.com>
    (cherry picked from commit 0dabfc8f5c7eff76a0d84b854e6215197a30cee8)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/gpu/drm/msm/dp/dp_ctrl.h
+++ drivers/gpu/drm/msm/dp/dp_ctrl.h
@@ -18,7 +18,7 @@ struct msm_dp_ctrl {
 struct phy;
 
 int msm_dp_ctrl_on_link(struct msm_dp_ctrl *msm_dp_ctrl);
-int msm_dp_ctrl_on_stream(struct msm_dp_ctrl *msm_dp_ctrl);
+int msm_dp_ctrl_on_stream(struct msm_dp_ctrl *msm_dp_ctrl, struct msm_dp_panel *msm_dp_panel);
 int msm_dp_ctrl_prepare_stream_on(struct msm_dp_ctrl *msm_dp_ctrl, bool force_link_train);
 void msm_dp_ctrl_off_link_stream(struct msm_dp_ctrl *msm_dp_ctrl);
 void msm_dp_ctrl_off_link(struct msm_dp_ctrl *msm_dp_ctrl);
@@ -42,7 +42,8 @@ void msm_dp_ctrl_config_psr(struct msm_dp_ctrl *msm_dp_ctrl);
 int msm_dp_ctrl_core_clk_enable(struct msm_dp_ctrl *msm_dp_ctrl);
 void msm_dp_ctrl_core_clk_disable(struct msm_dp_ctrl *msm_dp_ctrl);
 
-void msm_dp_ctrl_clear_vsc_sdp_pkt(struct msm_dp_ctrl *msm_dp_ctrl);
+void msm_dp_ctrl_clear_vsc_sdp_pkt(struct msm_dp_ctrl *msm_dp_ctrl,
+		struct msm_dp_panel *msm_dp_panel);
 void msm_dp_ctrl_psm_config(struct msm_dp_ctrl *msm_dp_ctrl);
 void msm_dp_ctrl_reinit_phy(struct msm_dp_ctrl *msm_dp_ctrl);
 
