f9ed74f42c9892497a49d5adaaeffad218a85ab1
Abhinav Kumar <quic_abhinavk@quicinc.com>
2024-10-07 12:21:53 -0700

    FROMLIST: drm/msm/dp: add support to send vcpf packets in dp controller
    VC payload fill sequence is inserted by the DP controller in the
    absence of stream symbols that is before stream is disabled. Add
    support to send the VCPF sequence for msm dp controller.
    
    Change-Id: I759badab9d66081149483ef1ee497ff9b88ba1e6
    Link: https://lore.kernel.org/all/20241205-dp_mst-v1-22-f8618d42a99a@quicinc.com/#t
    Signed-off-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Signed-off-by: Yongxing Mou <quic_yongmou@quicinc.com>
    (cherry picked from commit 969e3798ab86e42e1de7e853081b6d225cdae0b2)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -1664,13 +1664,13 @@ void msm_dp_display_atomic_disable(struct msm_dp *dp)
 
 	msm_dp_display = container_of(dp, struct msm_dp_display_private, msm_dp_display);
 
-	msm_dp_ctrl_push_idle(msm_dp_display->ctrl);
-
 	if (dp->mst_active) {
+		msm_dp_ctrl_push_vcpf(msm_dp_display->ctrl, msm_dp_display->panel);
 		msm_dp_ctrl_mst_stream_channel_slot_setup(msm_dp_display->ctrl,
 							  msm_dp_display->max_stream);
 		msm_dp_ctrl_mst_send_act(msm_dp_display->ctrl);
-	}
+	} else
+		msm_dp_ctrl_push_idle(msm_dp_display->ctrl);
 }
 
 static void msm_dp_display_unprepare(struct msm_dp_display_private *dp)
