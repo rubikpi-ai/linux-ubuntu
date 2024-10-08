a95e0e59ef43aebe6eef6ab44fcb358a328ff7de
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-10-08 11:47:37 -0700

    phonet: Handle error of rtnl_register_module().
    BugLink: https://bugs.launchpad.net/bugs/2097301
    
    [ Upstream commit b5e837c86041bef60f36cf9f20a641a30764379a ]
    
    Before commit addf9b90de22 ("net: rtnetlink: use rcu to free rtnl
    message handlers"), once the first rtnl_register_module() allocated
    rtnl_msg_handlers[PF_PHONET], the following calls never failed.
    
    However, after the commit, rtnl_register_module() could fail silently
    to allocate rtnl_msg_handlers[PF_PHONET][msgtype] and requires error
    handling for each call.
    
    Handling the error allows users to view a module as an all-or-nothing
    thing in terms of the rtnetlink functionality.  This prevents syzkaller
    from reporting spurious errors from its tests, where OOM often occurs
    and module is automatically loaded.
    
    Let's use rtnl_register_many() to handle the errors easily.
    
    Fixes: addf9b90de22 ("net: rtnetlink: use rcu to free rtnl message handlers")
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Acked-by: Rémi Denis-Courmont <courmisch@gmail.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>

--- net/phonet/pn_netlink.c
+++ net/phonet/pn_netlink.c
@@ -284,23 +284,17 @@ out:
 	return skb->len;
 }
 
+static const struct rtnl_msg_handler phonet_rtnl_msg_handlers[] __initdata_or_module = {
+	{THIS_MODULE, PF_PHONET, RTM_NEWADDR, addr_doit, NULL, 0},
+	{THIS_MODULE, PF_PHONET, RTM_DELADDR, addr_doit, NULL, 0},
+	{THIS_MODULE, PF_PHONET, RTM_GETADDR, NULL, getaddr_dumpit, 0},
+	{THIS_MODULE, PF_PHONET, RTM_NEWROUTE, route_doit, NULL, 0},
+	{THIS_MODULE, PF_PHONET, RTM_DELROUTE, route_doit, NULL, 0},
+	{THIS_MODULE, PF_PHONET, RTM_GETROUTE, NULL, route_dumpit,
+	 0},
+};
+
 int __init phonet_netlink_register(void)
 {
-	int err = rtnl_register_module(THIS_MODULE, PF_PHONET, RTM_NEWADDR,
-				       addr_doit, NULL, 0);
-	if (err)
-		return err;
-
-	/* Further rtnl_register_module() cannot fail */
-	rtnl_register_module(THIS_MODULE, PF_PHONET, RTM_DELADDR,
-			     addr_doit, NULL, 0);
-	rtnl_register_module(THIS_MODULE, PF_PHONET, RTM_GETADDR,
-			     NULL, getaddr_dumpit, 0);
-	rtnl_register_module(THIS_MODULE, PF_PHONET, RTM_NEWROUTE,
-			     route_doit, NULL, 0);
-	rtnl_register_module(THIS_MODULE, PF_PHONET, RTM_DELROUTE,
-			     route_doit, NULL, 0);
-	rtnl_register_module(THIS_MODULE, PF_PHONET, RTM_GETROUTE,
-			     NULL, route_dumpit, 0);
-	return 0;
+	return rtnl_register_many(phonet_rtnl_msg_handlers);
 }
