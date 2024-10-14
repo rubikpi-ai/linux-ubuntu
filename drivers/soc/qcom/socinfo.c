b08a16bb5e9ecb80d67846f615dc7d7bfab39df0
Sankalp Negi <quic_snegi@quicinc.com>
2024-10-14 01:54:38 -0700

    PENDING: soc: qcom: socinfo: add support for SA7255P SoC
    Add SocInfo support for SA7255P SoC.
    
    Change-Id: I843552e2ccc140a0a7a94f9974833e42a3ac1fae
    Signed-off-by: Sankalp Negi <quic_snegi@quicinc.com>
    (cherry picked from commit 858b6fa4b0a39a0203d47ee60aad41babf4dbe5a)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -442,6 +442,7 @@ static const struct soc_id soc_id[] = {
 	{ qcom_board_id(IPQ5302) },
 	{ qcom_board_id(QCS8550) },
 	{ qcom_board_id(QCM8550) },
+	{ qcom_board_id(SA7255P) },
 	{ qcom_board_id(IPQ5300) },
 	{ qcom_board_id(IPQ5321) },
 	{ qcom_board_id(QCS9100) },
