9e2d90153dfcbe1249bf22fb5511681cd357456f
Xiangxu Yin <quic_xiangxuy@quicinc.com>
2024-11-22 16:31:36 +0800

    FROMLIST: drm/msm/dp: Add DisplayPort support for QCS615
    The Qualcomm QCS615 platform comes with a DisplayPort controller use the
    same base offset as sc7180. add support for this in DP driver.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Change-Id: Ia90c4f7b568cbb1e2327a62d8a300130f942c681
    Signed-off-by: Xiangxu Yin <quic_xiangxuy@quicinc.com>
    Link: https://lore.kernel.org/all/20241129-add-displayport-support-for-qcs615-platform-v1-4-09a4338d93ef@quicinc.com/
    Signed-off-by: Yongxing Mou <yongxing.mou@oss.qualcomm.com>
    (cherry picked from commit 5ab6b516b5491a15f705d94d7d23984f1b7670bc)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -232,6 +232,7 @@ static const struct of_device_id msm_dp_dt_match[] = {
 	{ .compatible = "qcom,sc8280xp-dp", .data = &msm_dp_desc_sc8280xp },
 	{ .compatible = "qcom,sc8280xp-edp", .data = &msm_dp_desc_sc8280xp },
 	{ .compatible = "qcom,sdm845-dp", .data = &msm_dp_desc_sc7180 },
+	{ .compatible = "qcom,sm6150-dp", .data = &msm_dp_desc_sc7180 },
 	{ .compatible = "qcom,sm8350-dp", .data = &msm_dp_desc_sc7180 },
 	{ .compatible = "qcom,sm8650-dp", .data = &msm_dp_desc_sm8650 },
 	{ .compatible = "qcom,x1e80100-dp", .data = &msm_dp_desc_x1e80100 },
