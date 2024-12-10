0d6a731b5fbc8361f59edff57fa2dfff6ed3c921
Ayushi Makhija <quic_amakhija@quicinc.com>
2024-12-10 12:10:19 +0530

    PENDING: drm/bridge: anx7625: update bridge_ops and sink detect logic
    In the anx7625_link_bridge function, we verify if a specific device
    is a panel bridge. If it is not, we add DRM_BRIDGE_OP_HPD or
    DRM_BRIDGE_OP_DETECT to the bridge operations. However, on port 1
    of the anx7625 bridge, any device added is always treated as a
    panel bridge, which prevents our connector_detect function from
    being called. To address this, instead of merely checking whether
    it is a panel bridge. we should verify the type of the panel
    bridge—whether it is a DisplayPort or eDP panel. If the panel
    bridge is a DisplayPort type, then we should add
    DRM_BRIDGE_OP_HPD or DRM_BRIDGE_OP_DETECT to the bridge
    operations.
    
    In the anx7625_sink_detect function, it checks if the device
    is a panel bridge and always sends a "connected" statusto the
    connector. When adding the DP port on port 1 of the anx7625,
    it incorrectly treats it as a panel bridge and sends an
    always "connected" status. Instead of checking the status
    on the panel bridge, it's better to check the hpd_status
    for connectors like DisplayPort. This way, it verifies the
    hpd_status variable before sending the status to the
    connector. For other connectors like eDP panels, the
    hpd_status variable will always be greater than 0.
    
    Change-Id: I26fdce1847f0785d32f198c11e927cf529912189
    Signed-off-by: Ayushi Makhija <quic_amakhija@quicinc.com>
    (cherry picked from commit a34ee2effc0ae19a033be08cbbee38a846c1b4eb)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/gpu/drm/bridge/analogix/anx7625.c
+++ drivers/gpu/drm/bridge/analogix/anx7625.c
@@ -1826,9 +1826,6 @@ static enum drm_connector_status anx7625_sink_detect(struct anx7625_data *ctx)
 
 	DRM_DEV_DEBUG_DRIVER(dev, "sink detect\n");
 
-	if (ctx->pdata.panel_bridge)
-		return connector_status_connected;
-
 	return ctx->hpd_status ? connector_status_connected :
 				     connector_status_disconnected;
 }
@@ -2632,7 +2629,7 @@ static int anx7625_link_bridge(struct drm_dp_aux *aux)
 	platform->bridge.of_node = dev->of_node;
 	if (!anx7625_of_panel_on_aux_bus(dev))
 		platform->bridge.ops |= DRM_BRIDGE_OP_EDID;
-	if (!platform->pdata.panel_bridge)
+	if (platform->pdata.panel_bridge->type == DRM_MODE_CONNECTOR_DisplayPort)
 		platform->bridge.ops |= DRM_BRIDGE_OP_HPD |
 					DRM_BRIDGE_OP_DETECT;
 	platform->bridge.type = platform->pdata.panel_bridge ?
