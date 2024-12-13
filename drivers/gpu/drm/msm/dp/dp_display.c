eb371bf2b0b2717f07d76366d88d3c6a6b38eaf9
Yongxing Mou <quic_yongmou@quicinc.com>
2024-12-13 16:03:01 +0800

    PENDING: drm/msm/dp: populate the max_streams for qcs8300 mst controller
    Enable mst for qcs8300, it use same dpu with sa8775p, so
    we can reuse the stream_intf_map, but not enable controller_1
    for it only have one controller.
    
    Change-Id: I0c8bea73a0e06eb7fb420b6fa8399bc906f510b5
    Signed-off-by: Yongxing Mou <quic_yongmou@quicinc.com>
    (cherry picked from commit 7f8149525144498276c6cc34c62458673352376c)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -150,8 +150,9 @@ static const unsigned int stream_intf_map_sa_8775p[][DP_STREAM_MAX] = {
 };
 
 static const struct msm_dp_desc msm_dp_desc_qcs8300[] = {
-	{ .io_start = 0xaf54000, .id = MSM_DP_CONTROLLER_0, .wide_bus_supported = true },
-	{ .io_start = 0xaf5c000, .id = MSM_DP_CONTROLLER_1, .wide_bus_supported = true },
+	{ .io_start = 0x0af54000, .id = MSM_DP_CONTROLLER_0, .wide_bus_supported = true,
+	  .max_streams = 2, .intf_map = stream_intf_map_sa_8775p[MSM_DP_CONTROLLER_0],
+	},
 	{}
 };
 
