ca3c0ecbe1f1856cf6a4e98e8f214036e737875f
Patrick Daly <quic_pdaly@quicinc.com>
2025-01-16 13:24:11 -0800

    QCLINUX: iommu/virtio-iommu: Move 'struct viommu_dev' to common header
    The arm-smmu-v3-qcom-virtio driver will make use of this in future
    patches.
    
    Change-Id: If6b5e0d8844d2f862170276ef6eea2ee828ea2eb
    Signed-off-by: Patrick Daly <quic_pdaly@quicinc.com>
    (cherry picked from commit 3e53aaf5d0b430dc90f16227e3be2c06415f8841
    https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

---
+++ include/linux/virtio-iommu.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+#ifndef _LINUX_VIRTIO_IOMMU_QCOM_H
+#define _LINUX_VIRTIO_IOMMU_QCOM_H
+
+#include <linux/iommu.h>
+#include <linux/virtio.h>
+#include <uapi/linux/virtio_iommu.h>
+
+#define VIOMMU_REQUEST_VQ		0
+#define VIOMMU_EVENT_VQ			1
+#define VIOMMU_NR_VQS			2
+
+struct viommu_dev {
+	struct iommu_device		iommu;
+	struct device			*dev;
+	struct virtio_device		*vdev;
+
+	struct ida			domain_ids;
+
+	struct virtqueue		*vqs[VIOMMU_NR_VQS];
+	spinlock_t			request_lock;
+	struct list_head		requests;
+	void				*evts;
+
+	/* Device configuration */
+	struct iommu_domain_geometry	geometry;
+	u64				pgsize_bitmap;
+	u32				first_domain;
+	u32				last_domain;
+	/* Supported MAP flags */
+	u32				map_flags;
+	u32				probe_size;
+};
+#endif /* _LINUX_VIRTIO_IOMMU_QCOM_H */
+
