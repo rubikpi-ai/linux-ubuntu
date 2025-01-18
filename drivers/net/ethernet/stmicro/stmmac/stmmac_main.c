d253aaf496129fcaf18cd166c9810c43553eb0ad
Abhishek Chauhan <quic_abchauha@quicinc.com>
2025-01-18 15:05:52 -0800

    PENDING: net: stmmac: Add support for 5GbaseR to configure mac
    5GbaseR needs different programming sequence for configuration.
    IOmacro configuration enables the iopads to put the configuration
    in 5G mode. MAC registers are configured accordingly when the
    phylink calls link up to the mac block.
    
    Change-Id: Ic30bddc58780d8a6cf4eb2de471c1e68bfc7f6bc
    Signed-off-by: Abhishek Chauhan <quic_abchauha@quicinc.com>
    (cherry picked from commit 40237d451dd9a0f1c337229f72a7455016a57aab)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1052,6 +1052,26 @@ static void stmmac_mac_link_up(struct phylink_config *config,
 		default:
 			return;
 		}
+	} else if (interface == PHY_INTERFACE_MODE_5GBASER) {
+		switch (speed) {
+		case SPEED_5000:
+			ctrl |= priv->hw->link.xgmii.speed5000;
+			break;
+		case SPEED_2500:
+			ctrl |= priv->hw->link.xgmii.speed2500;
+			break;
+		case SPEED_1000:
+			ctrl |= priv->hw->link.speed1000;
+			break;
+		case SPEED_100:
+			ctrl |= priv->hw->link.speed100;
+			break;
+		case SPEED_10:
+			ctrl |= priv->hw->link.speed10;
+			break;
+		default:
+			return;
+		}
 	} else {
 		switch (speed) {
 		case SPEED_2500:
