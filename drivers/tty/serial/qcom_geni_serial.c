69e426f6ce9233a1d9eeb1500860259280f6ad2e
Panicker Harish <quic_pharish@quicinc.com>
2025-02-12 11:12:13 +0530

    PENDING: tty: serial: qcom-geni-serial: fix CTS control for UART in loopback mode
    When running loopback with flow_control on and off,
    uart_stop_tx is called due to a change in hw_stopped
    from the serial core, resulting in no data transfer.
    Consequently, the loopback remains in a wait state
    for the transfer to complete.
    
    To address this, set the TIOCM_CTS(sw-assisted CTS
    flow control flag) control signal when the UART port
    is in loopback mode. This ensures proper flow control.
    
    Change-Id: I97f412ea6f9d10cca9863422b99d98f98cf8a953
    Signed-off-by: Panicker Harish <quic_pharish@quicinc.com>
    (cherry picked from commit 4f9315f1af87cbcfc0d458a6fc6b01b42feedd64)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/tty/serial/qcom_geni_serial.c
+++ drivers/tty/serial/qcom_geni_serial.c
@@ -226,13 +226,14 @@ static void qcom_geni_serial_config_port(struct uart_port *uport, int cfg_flags)
 static unsigned int qcom_geni_serial_get_mctrl(struct uart_port *uport)
 {
 	unsigned int mctrl = TIOCM_DSR | TIOCM_CAR;
+	struct qcom_geni_serial_port *port = to_dev_port(uport);
 	u32 geni_ios;
 
 	if (uart_console(uport)) {
 		mctrl |= TIOCM_CTS;
 	} else {
 		geni_ios = readl(uport->membase + SE_GENI_IOS);
-		if (!(geni_ios & IO2_DATA_IN))
+		if (!(geni_ios & IO2_DATA_IN) || port->loopback)
 			mctrl |= TIOCM_CTS;
 	}
 
