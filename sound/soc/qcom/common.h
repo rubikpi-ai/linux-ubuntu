9d87145530d72822ff887f452ea595dc9860dfea
Mohammad Rafi Shaik <quic_mohs@quicinc.com>
2025-02-17 06:40:56 +0530

    QCLINUX: ASoC: qcom: common: Add qcom_snd_parse_of_v2 support
    Add qcom_snd_parse_of_v2 function support, which will help to
    parse and configure DAI format and mclk configurations.
    
    Change-Id: Ic6f043c5beb1c5270e11d787c495cc21669db65b
    Signed-off-by: Mohammad Rafi Shaik <quic_mohs@quicinc.com>
    (cherry picked from commit 516f9813a053199478066fb3c1293547c60a49d8)
    [ asoc_dummy_dlc -> snd_soc_dummy_dlc
      asoc_simple_parse_daifmt -> simple_util_parse_daifmt ]
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- sound/soc/qcom/common.h
+++ sound/soc/qcom/common.h
@@ -1,12 +1,24 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 // Copyright (c) 2018, The Linux Foundation. All rights reserved.
+// Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
 
 #ifndef __QCOM_SND_COMMON_H__
 #define __QCOM_SND_COMMON_H__
 
 #include <sound/soc.h>
 
+struct qcom_snd_dailink_data {
+	u32 mclk_fs;
+	u32 mclk_id;
+	u32 clk_direction;
+};
+
+struct qcom_snd_common_data {
+	struct qcom_snd_dailink_data *link_data;
+};
+
 int qcom_snd_parse_of(struct snd_soc_card *card);
+int qcom_snd_parse_of_v2(struct snd_soc_card *card, struct qcom_snd_common_data *priv);
 int qcom_snd_wcd_jack_setup(struct snd_soc_pcm_runtime *rtd,
 			    struct snd_soc_jack *jack, bool *jack_setup);
 
