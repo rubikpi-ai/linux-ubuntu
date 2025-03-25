20f26a7d968fb4ffeaf9e8d1b01389a78cf53582
Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
2025-03-25 11:40:55 +0530

    FROMLIST: soc: qcom: socinfo: Add support for new fields in revision 21
    Add the subpartfeature offset field to the socinfo structure
    which came for version 21 of socinfo structure.
    
    Change-Id: I98ffb914d66fa1819d3b188f72a90398f3636353
    Link: https://lore.kernel.org/lkml/20250411095025.4067964-2-mukesh.ojha@oss.qualcomm.com/
    Signed-off-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    Signed-off-by: Yadu MG <ymg@qti.qualcomm.com>
    (cherry picked from commit ce0c44b6f626b6a402da58aa31cd0a84f29a47b8
    https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -159,6 +159,7 @@ struct socinfo_params {
 	u32 boot_cluster;
 	u32 boot_core;
 	u32 raw_package_type;
+	u32 nsubpart_feat_array_offset;
 };
 
 struct smem_image_version {
@@ -610,6 +611,7 @@ static void socinfo_debugfs_init(struct qcom_socinfo *qcom_socinfo,
 			   &qcom_socinfo->info.fmt);
 
 	switch (qcom_socinfo->info.fmt) {
+	case SOCINFO_VERSION(0, 21):
 	case SOCINFO_VERSION(0, 20):
 		qcom_socinfo->info.raw_package_type = __le32_to_cpu(info->raw_package_type);
 		debugfs_create_u32("raw_package_type", 0444, qcom_socinfo->dbg_root,
