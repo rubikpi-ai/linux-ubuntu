4e8dc69904d83717f026425bfe175b78f77c1c11
Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
2025-03-25 11:54:57 +0530

    FROMLIST: soc: qcom: socinfo: Add support for new fields in revision 22
    Add the ncluster_cores_array_offset field with socinfo structure
    revision 22 which specifies no of cores present in each cluster.
    
    Change-Id: Ia2c235a8ef84858b365a0b581d717c87465a4587
    Link: https://lore.kernel.org/lkml/20250411095025.4067964-3-mukesh.ojha@oss.qualcomm.com/
    Signed-off-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    Signed-off-by: Yadu MG <ymg@qti.qualcomm.com>
    (cherry picked from commit 131639ea76fa829d724f87134c19ef7386e242d9
    https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -160,6 +160,7 @@ struct socinfo_params {
 	u32 boot_core;
 	u32 raw_package_type;
 	u32 nsubpart_feat_array_offset;
+	u32 ncluster_cores_array_offset;
 };
 
 struct smem_image_version {
@@ -611,6 +612,7 @@ static void socinfo_debugfs_init(struct qcom_socinfo *qcom_socinfo,
 			   &qcom_socinfo->info.fmt);
 
 	switch (qcom_socinfo->info.fmt) {
+	case SOCINFO_VERSION(0, 22):
 	case SOCINFO_VERSION(0, 21):
 	case SOCINFO_VERSION(0, 20):
 		qcom_socinfo->info.raw_package_type = __le32_to_cpu(info->raw_package_type);
