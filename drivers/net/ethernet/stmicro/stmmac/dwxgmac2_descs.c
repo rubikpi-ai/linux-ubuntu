cd39bab9474aa6f7c8f0b769446187dbca02a2e2
Abhishek Chauhan <quic_abchauha@quicinc.com>
2025-04-08 18:49:46 -0700

    PENDING: net: stmmac: Enable L2 split header support on XGMAC
    XGMAC driver doesn't support L2 level split, enable the MAC extended
    configuration register to support L2 split and enable AV split for
    tagged AV packets.
    
    Change-Id: I89f8efad80703c045529f1f7452dbe775de838bd
    Signed-off-by: Abhishek Chauhan <quic_abchauha@quicinc.com>
    (cherry picked from commit ad20099a3564f18c065bdfa21a49f80562c972d9)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/net/ethernet/stmicro/stmmac/dwxgmac2_descs.c
+++ drivers/net/ethernet/stmicro/stmmac/dwxgmac2_descs.c
@@ -285,6 +285,8 @@ static void dwxgmac2_get_rx_header_len(struct dma_desc *p, unsigned int *len)
 {
 	if (le32_to_cpu(p->des3) & XGMAC_RDES3_L34T)
 		*len = le32_to_cpu(p->des2) & XGMAC_RDES2_HL;
+	else if (le32_to_cpu(p->des3) & XGMAC_RDES3_L2T)
+		*len = (le32_to_cpu(p->des2) & XGMAC_RDES2_NONIPHL) >> 2;
 }
 
 static void dwxgmac2_set_sec_addr(struct dma_desc *p, dma_addr_t addr, bool is_valid)
