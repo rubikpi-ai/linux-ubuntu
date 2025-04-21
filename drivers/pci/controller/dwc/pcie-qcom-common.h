23fd8c82f86f70aa8b2bd1d48bc4f8fb9a539744
Ziyue Zhang <quic_ziyuzhan@quicinc.com>
2025-04-21 15:59:08 +0800

    PENDING: PCI: qcom: Add equalization settings for 8.0 GT/s
    Adding lane equalization setting for 8.0 GT/s to enhance link stability
    and fix AER correctable errors reported on some platforms (eg. SA8775P).
    
    GEN3 and GEN4 require the same equalization setting. This setting is
    programmed into a group of shadow registers, which can be switched to
    configure equalization for different GEN speeds by writing 00b, 01b
    to `RATE_SHADOW_SEL`.
    
    Hence program equalization registers in a loop using link speed as index,
    so that equalization setting can be programmed for both GEN3 and GEN4.
    
    Change-Id: I62e2486615de8c079bfd2f1b0f1fdb500dedb701
    Co-developed-by: Qiang Yu <quic_qianyu@quicinc.com>
    Signed-off-by: Qiang Yu <quic_qianyu@quicinc.com>
    Signed-off-by: Ziyue Zhang <quic_ziyuzhan@quicinc.com>
    (cherry picked from commit bfd13fbd21b52ffaf2a5bf3bf5e902a916c237a2)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/pci/controller/dwc/pcie-qcom-common.h
+++ drivers/pci/controller/dwc/pcie-qcom-common.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2024-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _PCIE_QCOM_COMMON_H
@@ -8,7 +8,7 @@
 
 struct dw_pcie;
 
-void qcom_pcie_common_set_16gt_equalization(struct dw_pcie *pci);
+void qcom_pcie_common_set_equalization(struct dw_pcie *pci);
 void qcom_pcie_common_set_16gt_lane_margining(struct dw_pcie *pci);
 
 #endif
