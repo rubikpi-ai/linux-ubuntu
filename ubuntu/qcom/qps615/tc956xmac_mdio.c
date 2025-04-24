ea9a2fea5b7122d3a6276561e4aee67bcc4ed31b
Masahiro Yamada <masahiro.yamada@canonical.com>
2025-04-24 19:21:39 +0900

    Ubuntu: SAUCE: change phy detection log-level from alert to info
    This is not an error.
    
    BugLink: https://bugs.launchpad.net/bugs/2103491 ("[6490][x02] tc956x_pci-eth errors found in dmesg")
    
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- ubuntu/qcom/qps615/tc956xmac_mdio.c
+++ ubuntu/qcom/qps615/tc956xmac_mdio.c
@@ -792,7 +792,7 @@ int tc956xmac_mdio_register(struct net_device *ndev)
 			if (phy_reg_read1 != -EBUSY && phy_reg_read2 != -EBUSY) {
 				phy_id = ((phy_reg_read1 << 16) | phy_reg_read2);
 				if (phy_id != 0x00000000 && phy_id != 0xffffffff) {
-					NMSGPR_ALERT(priv->device,
+					NMSGPR_INFO(priv->device,
 							"TC956X: [2] Phy detected C45 at ID/ADDR %d\n", addr);
 
 #else
