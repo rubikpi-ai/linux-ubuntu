651c38c42d3c943cfe3902c3859b396adb6b7e50
karthik sanagavarapu <quic_kartsana@quicinc.com>
2025-05-21 17:14:22 +0530

    PENDING: misc: fastrpc: Resolve compilation warnings
    This change corrects pointer/integer casts and remove unused variable.
    Resolve -Wunused-value and -Wint-conversion warnings for improved
    code safety and clarity.
    
    Change-Id: Id75dae209089133e2c8024881a7c9ad41df0a69c
    Signed-off-by: karthik sanagavarapu <quic_kartsana@quicinc.com>
    (cherry picked from commit de17fca08f661b6b02b15c7c585e7168d103e1a2
    https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/misc/fastrpc.c
+++ drivers/misc/fastrpc.c
@@ -1378,7 +1378,7 @@ static void fastrpc_update_invoke_count(u32 handle, u64 *perf_counter,
 
 	count = GET_COUNTER(perf_counter, PERF_COUNT);
 	if (count)
-		*count++;
+		(*count)++;
 }
 
 static int poll_for_remote_response(struct fastrpc_invoke_ctx *ctx, u64 timeout)
@@ -2123,7 +2123,7 @@ static int fastrpc_copy_args(struct fastrpc_invoke *inv)
 			return -EFAULT;
 		}
 	}
-	inv->args = args;
+	inv->args = (u64)args;
 
 	return 0;
 }
@@ -2132,7 +2132,6 @@ static int fastrpc_invoke(struct fastrpc_user *fl, char __user *argp)
 {
 	struct fastrpc_invoke_v2 ioctl = {0};
 	struct fastrpc_invoke inv;
-	u32 nscalars;
 	int err;
 
 	if (copy_from_user(&inv, argp, sizeof(inv)))
@@ -2144,7 +2143,7 @@ static int fastrpc_invoke(struct fastrpc_user *fl, char __user *argp)
 
 	ioctl.inv = inv;
 	err = fastrpc_internal_invoke(fl, false, &ioctl);
-	kfree(inv.args);
+	kfree((void *)inv.args);
 
 	return err;
 }
@@ -2162,7 +2161,7 @@ static int fastrpc_invokev2(struct fastrpc_user *fl, char __user *argp)
 		return err;
 
 	err = fastrpc_internal_invoke(fl, false, &inv2);
-	kfree(inv2.inv.args);
+	kfree((void *)inv2.inv.args);
 
 	return err;
 }
