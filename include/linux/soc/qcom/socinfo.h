079fad8a9090be92dfdfbc945beca3853f3ce59f
Naina Mehta <quic_nainmeht@quicinc.com>
2025-06-05 13:49:03 +0530

    QCLINUX: soc: qcom: socinfo: Add DDR info SMEM struct
    Add DDR info struct to socinfo. The struct can be used
    by clients to read DDR related information via SMEM.
    
    Change-Id: Ib3609ceba7c8155737caa3636af736873b0604bc
    Signed-off-by: Naina Mehta <quic_nainmeht@quicinc.com>
    (cherry picked from commit 110f22d0848a93391829d9563a74e6ee9fc3eac7
    https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- include/linux/soc/qcom/socinfo.h
+++ include/linux/soc/qcom/socinfo.h
@@ -12,6 +12,40 @@
 #define SMEM_SOCINFO_BUILD_ID_LENGTH	32
 #define SMEM_SOCINFO_CHIP_ID_LENGTH	32
 
+#define SMEM_DDR_BUILD_ID		603
+
+#define MAX_IDX_CH 8
+
+/* DDR info SMEM item structure */
+struct ddr_part_details {
+	u8 revision_id1[2];
+	u8 revision_id2[2];
+	u8 width[2];
+	u8 density[2];
+};
+
+struct ddr_freq_table {
+	__le32 freq_khz;
+	u8 enable;
+};
+
+struct ddr_freq_plan_entry {
+	struct ddr_freq_table ddr_freq[14];
+	u8 num_ddr_freqs;
+	__le32 *clk_period_address;
+	__le32 max_nom_ddr_freq;
+};
+
+struct ddrinfo {
+	u8 manuf_id;
+	u8 device_type;
+	struct ddr_part_details ddr_params[MAX_IDX_CH];
+	struct ddr_freq_plan_entry ddr_freq_tbl;
+	u8 num_channels;
+	u8 num_ranks[2];
+	u8 hbb[2][2];
+};
+
 /* Socinfo SMEM item structure */
 struct socinfo {
 	__le32 fmt;
