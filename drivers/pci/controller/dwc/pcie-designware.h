36f96f99be902bf3bfc5a9439661ddfcd12313f4
Mayank Rana <mayank.rana@oss.qualcomm.com>
2025-06-16 15:42:56 -0700

    BACKPORT: PCI: dwc: Export dwc MSI controller related APIs
    Export dw_pcie_msi_host_init(), dw_pcie_msi_init(), and dw_pcie_free_msi()
    APIs to allow dwc PCIe controller based MSI functionality from ECAM pcie
    driver. Move MSI IRQ related initialization code into dw_pcie_msi_init()
    as this code must be executed before dw_pcie_msi_init() API can be used
    with ECAM driver.
    
    Git-commit: a60d92f6d941bd77bf3aaec724a7c95857c0165b
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Change-Id: I47588b23f6ffc1c567cdad2d01b5b5417c1c0d47
    Signed-off-by: Mayank Rana <mayank.rana@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20250616224259.3549811-2-mayank.rana@oss.qualcomm.com
    Signed-off-by: Nitesh Gupta <nitegupt@qti.qualcomm.com>
    (cherry picked from commit 19aebabae4e08a82eb7c2a62d571d1f6e5ff4cee)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/pci/controller/dwc/pcie-designware.h
+++ drivers/pci/controller/dwc/pcie-designware.h
@@ -664,6 +664,9 @@ static inline enum dw_pcie_ltssm dw_pcie_get_ltssm(struct dw_pcie *pci)
 
 #ifdef CONFIG_PCIE_DW_HOST
 irqreturn_t dw_handle_msi_irq(struct dw_pcie_rp *pp);
+void dw_pcie_msi_init(struct dw_pcie_rp *pp);
+int dw_pcie_msi_host_init(struct dw_pcie_rp *pp);
+void dw_pcie_free_msi(struct dw_pcie_rp *pp);
 int dw_pcie_setup_rc(struct dw_pcie_rp *pp);
 int dw_pcie_host_init(struct dw_pcie_rp *pp);
 void dw_pcie_host_deinit(struct dw_pcie_rp *pp);
@@ -675,6 +678,17 @@ static inline irqreturn_t dw_handle_msi_irq(struct dw_pcie_rp *pp)
 {
 	return IRQ_NONE;
 }
+
+static inline void dw_pcie_msi_init(struct dw_pcie_rp *pp)
+{ }
+
+static inline int dw_pcie_msi_host_init(struct dw_pcie_rp *pp)
+{
+	return -ENODEV;
+}
+
+static inline void dw_pcie_free_msi(struct dw_pcie_rp *pp)
+{ }
 
 static inline int dw_pcie_setup_rc(struct dw_pcie_rp *pp)
 {
