ceae20c96f7482fd36ee79af5bbdc66eb6afddcf
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2025-07-18 15:53:57 -0700

    FROMLIST: net: stmmac: leave enabling _RE and _TE to stmmac_mac_link_up()
    We only need to enable the MAC receiver and transmiter only when the
    link has come up.
    
    With commit "net: stmmac: move phylink_resume() after resume setup
    is complete" we move the race between stmmac_mac_link_up() and
    stmmac_hw_setup(), ensuring that stmmac_mac_link_up() happens
    afterwards. This patch is a pre-requisit of this change.
    
    Remove the unnecessary call to stmmac_mac_set(, true) in
    stmmac_hw_setup().
    
    Tested-by: Furong Xu <0x1207@xxxxxxxxx>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Link: https://lore.kernel.org/all/E1uAqYN-002D41-53@rmk-PC.armlinux.org.uk/
    Change-Id: Ia1ff12ccd2269acfcf233792015be49057e25e72
    Signed-off-by: Sagar Cheluvegowda <quic_scheluve@quicinc.com>
    (cherry picked from commit 638102fa6423a8ef0628c6c2127ed9800e8a5377)
    (cherry picked from commit 7b378d9f63cda8c8f890863690bd39a11f1b2305
    https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -3507,9 +3507,6 @@ static int stmmac_hw_setup(struct net_device *dev, bool ptp_register)
 		priv->hw->rx_csum = 0;
 	}
 
-	/* Enable the MAC Rx/Tx */
-	stmmac_mac_set(priv, priv->ioaddr, true);
-
 	/* Set the HW DMA mode and the COE */
 	stmmac_dma_operation_mode(priv);
 
