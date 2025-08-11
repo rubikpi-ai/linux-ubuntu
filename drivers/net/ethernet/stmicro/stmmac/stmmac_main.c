a5953988be25f893b7d4fc12510e601d5a28e73f
Suraj Jaiswal <quic_jsuraj@quicinc.com>
2025-08-11 09:58:29 +0530

    PENDING: net: stmmac: start phylink after initialization
    start phylink resume after Ethernet initialization
    is completed. for the fix link case ethernet link will
    be up after phy_resume and if initialization not completed
    causing ping issue after S2D.
    
    Change-Id: I27ef56793c76252ef745c03cbdc9c42d32cd14d7
    Signed-off-by: Suraj Jaiswal <quic_jsuraj@quicinc.com>
    (cherry picked from commit 44704487d080740328f9b4d597ddbe0357c41ced
    https://git.codelinaro.org/clo/la/kernel/qcom kernel.qclinux.1.0.r1-rel)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -8158,16 +8158,6 @@ int stmmac_resume(struct device *dev)
 	}
 
 	rtnl_lock();
-	if (device_may_wakeup(priv->device) && priv->plat->pmt) {
-		phylink_resume(priv->phylink);
-	} else {
-		phylink_resume(priv->phylink);
-		if (device_may_wakeup(priv->device))
-			phylink_speed_up(priv->phylink);
-	}
-	rtnl_unlock();
-
-	rtnl_lock();
 	mutex_lock(&priv->lock);
 
 	stmmac_reset_queues_param(priv);
@@ -8192,6 +8182,13 @@ int stmmac_resume(struct device *dev)
 	stmmac_enable_all_dma_irq(priv);
 
 	mutex_unlock(&priv->lock);
+	if (device_may_wakeup(priv->device) && priv->plat->pmt) {
+		phylink_resume(priv->phylink);
+	} else {
+		phylink_resume(priv->phylink);
+		if (device_may_wakeup(priv->device))
+			phylink_speed_up(priv->phylink);
+	}
 	rtnl_unlock();
 
 	netif_device_attach(ndev);
