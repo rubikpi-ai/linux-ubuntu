b1812a418295287844df83a511d4e3ed476c33bd
Hui Wang <hui.wang@canonical.com>
2025-09-03 18:37:39 +0800

    UBUNTU: SAUCE: Do the necessary change to support adreno for -1056
    BugLink: https://bugs.launchpad.net/bugs/2115485 (The msm display driver not updated in the kernel)
    
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- drivers/gpu/drm/msm_default/msm_drv.c
+++ drivers/gpu/drm/msm_default/msm_drv.c
@@ -1038,6 +1038,10 @@ int msm_drv_probe(struct device *master_dev,
 			return ret;
 	}
 
+	ret = add_gpu_components(master_dev, &match);
+	if (ret)
+		return ret;
+
 	/* on all devices that I am aware of, iommu's which can map
 	 * any address the cpu can see are used:
 	 */
@@ -1086,6 +1090,7 @@ static int __init msm_drm_register(void)
 	msm_dsi_register();
 	msm_hdmi_register();
 	msm_dp_register();
+	adreno_register();
 	msm_mdp4_register();
 	msm_mdss_register();
 	return platform_driver_register(&msm_platform_driver);
@@ -1099,6 +1104,7 @@ static void __exit msm_drm_unregister(void)
 	msm_mdp4_unregister();
 	msm_dp_unregister();
 	msm_hdmi_unregister();
+	adreno_unregister();
 	msm_dsi_unregister();
 	msm_mdp_unregister();
 	msm_dpu_unregister();
