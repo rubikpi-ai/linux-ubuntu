18b8e37613298a0fdf2199a196ae9cc5d3c66fb9
Wen Gong <quic_wgong@quicinc.com>
2025-10-23 20:34:41 +0800

    BACKPORT: wifi: ath12k: store and send country code to firmware after recovery
    Currently ath12k does not send the country code to firmware after device
    recovery. As a result the country code will be the default one which
    is reported from firmware. Country code is important, so ath12k also
    need to restore it to the value which was used before recovery.
    
    This is only needed for platforms which support the current_cc_support
    hardware parameter.
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    
    Change-Id: Iba5d144ae3c8ba7f9f11b14fe79b59a63e14ea2e
    Signed-off-by: Wen Gong <quic_wgong@quicinc.com>
    Signed-off-by: Kang Yang <quic_kangyang@quicinc.com>
    Reviewed-by: Vasanthakumar Thiagarajan <vasanthakumar.thiagarajan@oss.qualcomm.com>
    Link: https://patch.msgid.link/20250401020840.357-5-quic_kangyang@quicinc.com
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>
    Signed-off-by: Yu Zhang(Yuriy) <yu.zhang@oss.qualcomm.com>
    (cherry picked from commit 1ee2a605400c9ca711811fcbae18ff3ac0282856)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- drivers/net/wireless/ath/ath12k/core.h
+++ drivers/net/wireless/ath/ath12k/core.h
@@ -577,6 +577,7 @@ struct ath12k {
 	u32 vdev_id_11d_scan;
 	struct completion completed_11d_scan;
 	enum ath12k_11d_state state_11d;
+	u8 alpha2[REG_ALPHA2_LEN];
 	bool regdom_set_by_user;
 
 	struct completion peer_assoc_done;
