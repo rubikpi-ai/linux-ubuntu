ff8000bd821418446a0208ff95a2d5b6f6e57e63
Yiftach Ariel <yariel@qti.qualcomm.com>
2025-11-19 09:51:26 +0100

    QCLINUX: firmware: qcom_scm_addon: remove QuIC Copyright
    Remove the QuIC Copyright line, as it's no longer required.
    
    Change-Id: I3a44a9e46a041229db28eb2bd7cc3c4305e082bb
    Signed-off-by: Yiftach Ariel <yariel@qti.qualcomm.com>
    (cherry picked from commit 53596b836cc15528b052ca19ce9e3dc8b39d3c12)
    Signed-off-by: Masahiro Yamada <masahiro.yamada@canonical.com>

--- include/linux/firmware/qcom/qcom_scm_addon.h
+++ include/linux/firmware/qcom/qcom_scm_addon.h
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
